46915150bf0a3fd1272e1dbb0111f279dd6de0af
Petr <poletaev.petr@gmail.com>
2017-05-14 04:02:27 +0700

    Create Calculator Test

--- src/calculator/test/CalculatorTest.java
+++ src/calculator/test/CalculatorTest.java
@@ -1,13 +1,10 @@
-package calculator.test;
-
-
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class CalculatorTest {
     @Test
     public final void newCalculator() {
-        Calculator  calculator= new Calculator();
+        Calculator calculator= new Calculator();
         Assertions.assertNotNull(calculator);
     }
 }

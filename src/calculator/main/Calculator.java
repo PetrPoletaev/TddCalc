1ae102c389903afa344c87c9a2b2b418b1792ffa
Petr <poletaev.petr@gmail.com>
2017-05-14 16:48:08 +0700

    Refactor add() for pass test add(x, y)

--- src/calculator/main/Calculator.java
+++ src/calculator/main/Calculator.java
@@ -5,7 +5,6 @@ public class Calculator {
 
     //todo реализовать метод add позднее до конца
     public double add(double x, double y) {
-        if(x==2&&y==3) return 5;
-            return 4;
+            return x+y;
     }
 }

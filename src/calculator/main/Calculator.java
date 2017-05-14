b857072589a8b010a78d5ff9923108f6b7ff2537
Petr <poletaev.petr@gmail.com>
2017-05-14 16:32:18 +0700

    Added add() trivial inmlementation

--- src/calculator/main/Calculator.java
+++ src/calculator/main/Calculator.java
@@ -2,4 +2,9 @@
  * Created by Petr on 14.05.2017.
  */
 public class Calculator {
+
+    //todo реализовать метод add позднее до конца
+    public double add(double x, double y) {
+        return 4;
+    }
 }

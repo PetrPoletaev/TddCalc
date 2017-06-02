27fc2cb408b9b78a09ff3903c58da613e64269ae
Petr <poletaev.petr@gmail.com>
2017-06-02 15:19:12 +0700

    Refactor mul()

--- src/calculator/main/Matrix.java
+++ src/calculator/main/Matrix.java
@@ -1,5 +1,19 @@
 public class Matrix {
-    public double[][] mul(double[][] a, double[][] b) {
-        return new double [][] {{-1,1},{2,2}, {3,3}};
+    public  double[][] mul( double[][] A, double[][] B ) {
+        int n = A[0].length;
+        double eps = 0.00001;
+        double[][] C = new double[ n ][ n ];
+
+        for( int i = 0; i < n; i++ ) {
+            for( int j = 0; j < n; j++ ) {
+                for( int k = 0; k < n; k++ ) {
+                    C[ i ][ j ] += A[ i ][ k ] * B[ k ][ j ];
+                    if( Math.abs(  0 - C[ i ][ j ] ) < eps ) {
+                        C[ i ][ j ] = 0;
+                    }
+                }
+            }
+        }
+        return C;
     }
 }
